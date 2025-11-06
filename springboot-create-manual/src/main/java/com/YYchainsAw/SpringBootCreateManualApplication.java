2ce0930eb1208057a7dabcf6d9e0a3c6e10d982b
YYchainsAw <2560634677@qq.com>
2025-11-06 21:26:36 +0800

    手动创建springboot

---
+++ springboot-create-manual/src/main/java/com/YYchainsAw/SpringBootCreateManualApplication.java
@@ -0,0 +1,16 @@
+package com.YYchainsAw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class SpringBootCreateManualApplication
+{
+    public static void main( String[] args ){
+        SpringApplication.run(SpringBootCreateManualApplication.class, args);
+    }
+}
