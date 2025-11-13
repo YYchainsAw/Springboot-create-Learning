e66c51d077c844f75ab67c31152b1990d36d2ec0
YYchainsAw <2560634677@qq.com>
2025-11-13 20:20:43 +0800

    注册条件

--- Springboot-register/src/main/java/com/yychainsaw/config/CommonConfig.java
+++ Springboot-register/src/main/java/com/yychainsaw/config/CommonConfig.java
@@ -2,20 +2,37 @@ package com.yychainsaw.config;
 
 import cn.itcast.pojo.Country;
 import cn.itcast.pojo.Province;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class CommonConfig {
 
+    //如果配置文件中包含country.name和country.system这两个属性，那么这个bean才会被创建
+    @ConditionalOnProperty(prefix = "country",name = {"name", "system"})
     @Bean
-    public Country country(){
-        return new Country();
+    public Country country(@Value("${country.name}") String name,@Value("${country.system}") String system){
+        Country country = new Country();
+
+        country.setName(name);
+        country.setSystem(system);
+
+        return country;
     }
 
     //对象默认的名字是方法名
     //如果方法的内部需要使用到ioc容器中已经存在的bean对象，那么只需要在方法上声明即可，spring会自动地注入
-    @Bean("aa")
+
+    //如果ioc容器中没有名字为aa的bean，那么这个bean才会被创建
+    //@ConditionalOnMissingBean(Country.class)
+    //@Bean("aa")
+    @Bean
+    //如果当前环境引入了web起步依赖，则环境中有DispatcherServlet，否则没有
+    @ConditionalOnClass(name = "org.springframework.web.servlet.DispatcherServlet")
     public Province province(){
         return new Province();
     }
--- Springboot-register/src/main/java/com/yychainsaw/springbootregister/SpringbootRegisterApplication.java
+++ Springboot-register/src/main/java/com/yychainsaw/springbootregister/SpringbootRegisterApplication.java
@@ -2,12 +2,10 @@ package com.yychainsaw.springbootregister;
 
 import cn.itcast.pojo.Country;
 import com.yychainsaw.anno.EnableCommoConfig;
-import com.yychainsaw.config.CommonConfig;
-import com.yychainsaw.config.CommonImportSelector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Import;
+
 
 
 /**
@@ -26,7 +24,7 @@ public class SpringbootRegisterApplication
         Country country = context.getBean(Country.class);
         System.out.println(country);
 
-        System.out.println(context.getBean("aa"));
+        System.out.println(context.getBean("province"));
     }
 
 
