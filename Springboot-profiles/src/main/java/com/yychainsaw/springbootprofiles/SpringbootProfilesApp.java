99220188a04a562567480de7e3139f4d7051dbba
YYchainsAw <2560634677@qq.com>
2025-11-20 19:23:03 +0800

    多环境开发

---
+++ Springboot-profiles/src/main/java/com/yychainsaw/springbootprofiles/SpringbootProfilesApp.java
@@ -0,0 +1,17 @@
+package com.yychainsaw.springbootprofiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class SpringbootProfilesApp
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(SpringbootProfilesApp.class, args);
+    }
+}
